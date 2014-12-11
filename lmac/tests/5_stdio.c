b255ca928083f7af89b992095698d6f0e3f027e8
Breckin Loggins <bloggins@greatfoundry.com>
2014-12-10 22:08:24 -0800

    Display fancy clang-like diagnostics
    Including the "caret"

--- lmac/tests/5_stdio.c
+++ lmac/tests/5_stdio.c
@@ -8,6 +8,8 @@
 
 #include "tests/c_prelude.h"
 //#include "stdio.h"
+#include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include/stdio.h"
+
 
 int main(int argc, char **argv) {
     char *what = "World";
