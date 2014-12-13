e5f8f8bb24c68fbf3f44c366abd20f21d105fb62
Breckin Loggins <bloggins@greatfoundry.com>
2014-12-12 22:52:04 -0800

    Interpreter: interpreted primitive values infrastructure

--- lmac/interp.c
+++ lmac/interp.c
@@ -14,6 +14,56 @@
 diag_printf(DIAG_ERROR, sl, __VA_ARGS__);                               \
 exit(ERR_INTERPRET);
 
+typedef enum {
+    VAL_INVALID,
+    
+    VAL_VOID,
+    VAL_CHAR,
+    VAL_INT,
+    VAL_STRING,
+} CIValueKind;
+
+typedef struct {
+    CIValueKind value_kind;
+    union {
+        char char_val;
+        int int_val;
+        char *str_val;
+    };
+} CIValue;
+
+CIValue *ci_val_create(CIValueKind kind) {
+    CIValue *v = (CIValue*)calloc(1, sizeof(CIValue));
+    v->value_kind = kind;
+    
+    return v;
+}
+
+CIValue *ci_void_val() {
+    return ci_val_create(VAL_VOID);
+}
+
+CIValue *ci_char_val(char c) {
+    CIValue *v = ci_val_create(VAL_CHAR);
+    v->char_val = c;
+    
+    return v;
+}
+
+CIValue *ci_int_val(int i) {
+    CIValue *v = ci_val_create(VAL_INT);
+    v->int_val = i;
+    
+    return v;
+}
+
+CIValue *ci_str_val(const char *s) {
+    CIValue *v = ci_val_create(VAL_STRING);
+    v->str_val = strdup(s);
+    
+    return v;
+}
+
 typedef struct CIContext {
     ASTBase *result;
 } CIContext;
@@ -40,10 +90,23 @@ int dispatch_visitor(ASTBase *node, VisitPhase phase, void *ctx) {
 #define CI_VISIT_FN(kind, type) int ci_visit_##kind(type *node, VisitPhase phase, CIContext *ctx)
 
 CI_VISIT_FN(AST_EXPR_NUMBER, ASTExprNumber) {
+    if (phase == VISIT_PRE) {
+        return VISIT_OK;
+    }
     
-    ctx->result = (ASTBase*)node;
+    AST_BASE(node)->visit_data = ci_int_val(node->number);
     
-    return VISIT_HANDLED;
+    return VISIT_OK;
+}
+
+CI_VISIT_FN(AST_EXPR_STRING, ASTExprString) {
+    if (phase == VISIT_PRE) {
+        return VISIT_OK;
+    }
+    
+    AST_BASE(node)->visit_data = ci_str_val(spelling_cstring(AST_BASE(node)->location.spelling));
+    
+    return VISIT_OK;
 }
 
 
@@ -57,6 +120,7 @@ void initialize_interp_data() {
 #   define CI_DATA(kind) interp_data[(kind)].visit = (VisitFn)ci_visit_##kind
     
     CI_DATA(AST_EXPR_NUMBER);
+    CI_DATA(AST_EXPR_STRING);
     
 }
 
@@ -73,6 +137,8 @@ bool interp_interpret(ASTBase *node, ASTBase **result) {
     if (result != NULL) {
         *result = ctx.result;
     }
+    
+    ast_visit_data_clean(node);
     return true;
 }
 
