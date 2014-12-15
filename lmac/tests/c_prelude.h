fe5050c6b6abd24a4eb9ea0df3e035fa37be6483
Breckin Loggins <bloggins@greatfoundry.com>
2014-12-15 10:24:38 -0800

    Node new error discovered while trying to use #pragma in the c_prelude

--- lmac/tests/c_prelude.h
+++ lmac/tests/c_prelude.h
@@ -6,6 +6,9 @@
 //  Copyright (c) 2014 Breckin Loggins. All rights reserved.
 //
 
+// TODO(bloggins): We get an error when we try to put this here
+// #pragma CLITE fcg_explicit_parens  /* make the code generator group all expressions explicitly */
+
 $$() void = $();
 $$() char = $c8;
 $$() int = $i32;
